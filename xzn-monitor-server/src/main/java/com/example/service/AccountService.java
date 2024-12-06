6747e3ec1107ec4944689ae090e757cb9a8e88d4
Nruonan <15231613@qq.com>
2024-12-06 10:17:56 +0800

    refactor(xzn-monitor-server):重构用户登录相关代码

--- xzn-monitor-server/src/main/java/com/example/service/AccountService.java
+++ xzn-monitor-server/src/main/java/com/example/service/AccountService.java
@@ -2,8 +2,9 @@ package com.example.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.entity.dto.AccountDO;
-import com.example.entity.vo.request.ConfirmResetReq;
-import com.example.entity.vo.request.EmailResetReq;
+
+import com.example.entity.vo.request.ConfirmResetDTOReq;
+import com.example.entity.vo.request.EmailResetDTOReq;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public interface AccountService extends IService<AccountDO>, UserDetailsService {
     AccountDO findAccountByNameOrEmail(String text);
     String registerEmailVerifyCode(String type, String email, String address);
-    String resetEmailAccountPassword(EmailResetReq info);
-    String resetConfirm(ConfirmResetReq info);
+    String resetEmailAccountPassword(EmailResetDTOReq info);
+    String resetConfirm(ConfirmResetDTOReq info);
 
     Boolean logout(HttpServletRequest request, HttpServletResponse response);
 }
